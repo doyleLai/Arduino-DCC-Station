729fceb72cc4fdb2a81669f33517664ce689f68b
doyleLai <laichunkiu@hotmail.com>
2022-01-02 01:25:20 +0800

    Add support to Arduino UNO WiFi Rev2

--- Arduino-DCC-Station/DCC_Signal.cpp
+++ Arduino-DCC-Station/DCC_Signal.cpp
@@ -44,14 +44,6 @@ static inline void Drive1(){
   PORTB &= ~(1 << 3);
 }
 
-ISR(TIMER2_COMPA_vect) {
-  handle_interrupt(TCNT2, OCR2A);
-}
-
-#else
-#error "Unsupported hardware"
-#endif
-
 static void handle_interrupt(volatile uint8_t & TCNTx, volatile uint8_t & OCRx){
   static DCC_pulse_state_t current_state = Preamble;
   static uint8_t timerValue = TIMER_SHORT;  // store last timer value
@@ -122,3 +114,122 @@ static void handle_interrupt(volatile uint8_t & TCNTx, volatile uint8_t & OCRx){
     OCRx = OCRx + timerValue;
   }
 }
+
+ISR(TIMER2_COMPA_vect) {
+  handle_interrupt(TCNT2, OCR2A);
+}
+
+#elif defined(ARDUINO_ARCH_MEGAAVR)
+
+//Timer frequency is 8Mhz (CLK_PER / 2)
+#define TIMER_SHORT 464 // 58usec pulse length
+#define TIMER_LONG 800 // 100usec pulse length
+
+static inline void Drive0();
+static inline void Drive1();
+static void handle_interrupt(volatile TCB_t & timer);
+
+// DCC Output Pins: pin 11 (PE.0) and 12 (PE.1)
+void SetupPins(){
+  pinMode(11, OUTPUT); //PORTE.DIRSET = 1<<0;
+  pinMode(12, OUTPUT); //PORTE.DIRSET = 1<<1;
+}
+
+// Uses TimerB0.
+ void SetupTimer() {
+  TCB0.CTRLB = TCB_CNTMODE_INT_gc; // Use Periodic Interrupt Mode
+  TCB0.CCMP = TIMER_SHORT; // 16 bit CCMP as Top value. 8MHz timer
+  TCB0.INTCTRL = TCB_CAPT_bm; // Enable the interrupt
+  TCB0.CTRLA = TCB_CLKSEL_CLKDIV2_gc | TCB_ENABLE_bm; // CLK_PER / 2 prescaler, enable timer
+}
+
+static inline void Drive0(){
+  VPORTE.OUT |= 0x01;
+  VPORTE.OUT &= ~0x02;
+}
+
+static inline void Drive1(){
+  VPORTE.OUT |= 0x02;
+  VPORTE.OUT &= ~0x01;
+}
+
+static void handle_interrupt(volatile TCB_t & timer){
+  static DCC_pulse_state_t current_state = Preamble;
+  static uint16_t timerValue = TIMER_SHORT;  // store last timer value
+  //static unsigned char flag = 0;              // used for short or long pulse
+  static bool isSecondPulse = false;
+  static uint8_t preamble_count = 16;
+  static unsigned char outbyte = 0;
+  static unsigned char cbit = 0x80;
+  static uint8_t byteIndex = 0;
+  static Packet cachedMsg = DCC_Packet_Generator::getDigitalDecoderIdlePacket();
+  static Packet resetPkt = DCC_Packet_Generator::getDigitalDecoderResetPacket();
+
+  // for every second interupt just toggle signal
+  if (isSecondPulse) {
+    Drive1();
+    isSecondPulse = false;
+    // set timer to last value
+    timer.CCMP = timerValue;
+    timer.INTFLAGS = TCB_CAPT_bm;
+
+  } else {  // != every second interrupt, advance bit or state
+    Drive0();
+    isSecondPulse = true;
+
+    switch (current_state) {
+      case Preamble:
+        timerValue = TIMER_SHORT; // This pulse is short 
+        preamble_count--;
+        if (preamble_count == 0) {  // advance to next state
+          // preamble completed, get next message
+          current_state = Seperator;
+          byteIndex = 0;  //start msg with byte 0
+          if (DCC.getSignalState()== SendPacket){
+            cachedMsg = DCC.getNextPacket();
+          }
+          else{
+            cachedMsg = resetPkt;
+          }
+        }
+        break;
+      case Seperator:
+        timerValue = TIMER_LONG; // This pulse is long 
+        // then advance to next state
+        current_state = SendByte;
+        // goto next byte ...
+        cbit = 0x80;  // send this bit next time first (0x80 = 1000 0000)
+        outbyte = cachedMsg.data[byteIndex];
+        break;
+      case SendByte:
+        if (outbyte & cbit) {
+          timerValue = TIMER_SHORT; // send short pulse
+        } else {
+          timerValue = TIMER_LONG; // send long pulse
+        }
+        cbit = cbit >> 1;
+        if (cbit == 0) {  // last bit sent, is there a next byte?
+          byteIndex++;
+          if (byteIndex >= cachedMsg.len) {
+            // this was already the XOR byte then advance to preamble
+            current_state = Preamble;
+            preamble_count = 16;
+          } else {
+            // send separtor and advance to next byte
+            current_state = Seperator;
+          }
+        }
+        break;
+    }
+    timer.CCMP = timerValue;
+    timer.INTFLAGS = TCB_CAPT_bm;
+  }
+}
+
+ISR(TCB0_INT_vect) {
+  handle_interrupt(TCB0);
+}
+#else
+#error "Unsupported hardware"
+#endif
+
--- README.md
+++ README.md
@@ -1,24 +1,27 @@
 # Arduino-DCC-Station
-This project adapted Michael Blank's program code for signal generation. The original program supports one decoder only. This program supports up to 9 decoders.
+This project adapted Michael Blank's [program code](http://www.oscale.net/en/simpledcc) for signal generation. The original program supports one decoder only. This program supports up to 9 decoders.
 
 The Arduino program generates two opposite DCC signals. When one signal goes HIGH, the other goes LOW, and vice versa. That means you can connect them to an H-bridge circuit to boost up the signal to power the track and locomotives.
 
-The program uses the hardware timer and interrupts to achieve the precise pulse width generations of the DCC signal. Therefore, this program is compatible with Arduino UNO and other ATmega328p based boards only.
+## Board Compatibility
+The program uses the hardware timer and interrupts to achieve the precise pulse width generations of the DCC signal.
 
-## Hardware wiring
-Connect the pin 11 and pin 12 on Arduino to the two input pins of the H-bridge. Connect the two output pins of the H-bridge to the track. Be sure you use an H-bridge to deliver the large current flow used by locomotives. Do not connect Arduino pins to the track and locomotives directly. 
+Supported boards:
+- Arduino UNO (Other ATmega328p based boards may be supported)
+- Arduino UNO WiFi Rev.2 (Other ATmega4809 based boards may be supported)
 
->If you use a non-isolated oscilloscope to measure the output pulses on Arduino or the motor driver, attach the probe tip only. Never connect the ground lead of a non-isolated oscilloscope to any signal pin.
+## Wiring
+Connect pin 11 and pin 12 on Arduino to the two input pins of the H-bridge. Connect the two output pins of the H-bridge to the track. Be sure you use an H-bridge to deliver the large current flow used by locomotives. Do not connect Arduino pins to the track and locomotives directly. 
 
-## How to use it
+## Installation
 1. Download and unzip the project.
 2. Open the folder "Arduino-DCC-Station" with Arduino IDE.
 3. Upload the program to your Arduino board.
-4. Wire the Arduino board, moter driver and the tracks. Make sure you have used an external power source to power up the motor driver and the tracks.
+4. Connect the Arduino to the H-bridge circuit and the track.
 5. Open Serial Monitor and send control messages (see below) to control your locos. 
 
 ## Control Messages
-The program continuously reads control messages from the USB serial port and generates corresponding DCC signals. The states of locomotives, including the direction, speed step and function ON/OFFs of F0-F20 (F0 refers to headlight), are stored in the SRAM so the system can re-generate DCC messages for all locomotives periodically without the need of repeating sending control messages.
+The program continuously reads control messages from the USB serial port and generates corresponding DCC signals. The states of locomotives, including the direction, speed step and function ON/OFFs of F0-F20 (F0 refers to headlight), are stored in the SRAM so the system can re-generate DCC messages for all locomotives periodically without the need of repetitive control messages.
 
 ### Message format
 Each message is enclosed by "<>" and has the following format:
@@ -62,4 +65,4 @@ Turns ON the F2 of address 3 decoder.
   * ON/OFF (E): 1
 
 ## Disclaimer
-You should take your own risk to use anything in this project. The author of the project is not responsible for any damage, hurt, or any kind of accident caused by the use of the program code and the suggested electronic components.
+You should take your own risk to use anything in this project. The author of the project is not responsible for any damage, hurt, or other kinds of accident caused by the use of the program code and the suggested electronic components.
